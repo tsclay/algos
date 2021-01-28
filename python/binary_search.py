0c199e9d2362aea5512b82a881bd0303f4233514
Tim <tsclay9@gmail.com>
2021-01-28 11:03:54 -0500

    corrected algo to reflect true binary search

--- python/binary_search.py
+++ python/binary_search.py
@@ -6,39 +6,49 @@ from math import floor
 
 
 def binary_search(list, target: int):
-    midpoint = floor(len(list) / 2)
+    midpoint = 0
     left = 0
     right = len(list) - 1
 
-    try:
+    if target == list[left]:
+        return left
+    if target == list[right]:
+        return right
+    if target == list[midpoint]:
+        return midpoint
 
-        if target == list[left]:
-            return left
-        if target == list[right]:
-            return right
-        if target == list[midpoint]:
+    if target > list[right] or target < list[left]:
+        return -1
+
+    while left <= right:
+        midpoint = (left + right) // 2
+
+        if list[midpoint] < target:
+            left = midpoint + 1
+
+        elif list[midpoint] > target:
+            right = midpoint - 1
+
+        else:
             return midpoint
 
-        if target >= list[midpoint]:
-            left = midpoint
+    # if target > list[midpoint]:
+    #     left = midpoint
 
-            while list[left] < target:
-                left += 1
+    #     while list[left] < target:
+    #         left += 1
 
-            if list[left] == target:
-                return left
+    #     if list[left] == target:
+    #         return left
 
-        elif target <= list[midpoint]:
-            right = midpoint
+    # elif target < list[midpoint]:
+    #     right = midpoint
 
-            while list[right] > target:
-                right -= 1
+    #     while list[right] > target:
+    #         right -= 1
 
-            if list[right] == target:
-                return right
-
-    except IndexError:
-        return -1
+    #     if list[right] == target:
+    #         return right
 
 
 sample_list = [1, 3, 4, 5, 6, 8, 9]
@@ -48,3 +58,4 @@ print(binary_search(sample_list, 3))  # 1
 print(binary_search(sample_list, 5))  # 3
 print(binary_search(sample_list, 9))  # 6
 print(binary_search(sample_list, 10))  # -1
+print(binary_search(sample_list, -1))  # -1
